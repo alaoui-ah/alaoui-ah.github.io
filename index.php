c38d950843e895b04b9a84221296723266f55c80
alaoui-ah <myahmed.elalaoui@usm.cl>
2024-11-23 01:12:34 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- index.html -->
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<!--https://www.jlab.org/Hall-B/secure/nuclear/CollabOct12/ahmed_NPWG_oct_2012.pdf -->
+
+<?php
+  date_default_timezone_set('America/Santiago');
+  date_default_timezone_get();
+  $mydate = $date = date('m/d/Y h:i:s a', time());
+/*
+  $file="index";
+  $fname3 = "./temp_file/login.log";
+  $fh3 = fopen($fname3, 'a');
+  fwrite($fh3,"$file, $mydate, $ipadd\n");
+  fclose($fh3);
+*/
+write("$mydate\n");
+?>
+
+<html>
+hhhhhhh
+</html>
+
